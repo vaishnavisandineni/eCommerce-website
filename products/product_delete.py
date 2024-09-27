437067802cc97696a9cae4a24448c458c9d51d76
vaishnavisandineni <vaishnavisandineni@gmail.com>
2024-09-27 23:36:48 +0530

    product_delete.py

---
+++ products/product_delete.py
@@ -0,0 +1,8 @@
+from flask import redirect, url_for
+from models import Product
+
+def product_delete(product_id):
+    product = Product.query.get_or_404(product_id)
+    db.session.delete(product)
+    db.session.commit()
+    return redirect(url_for("products"))
